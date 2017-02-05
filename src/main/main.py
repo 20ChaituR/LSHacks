eda6ae0c7537b6e8c2f6e5a010695d70492f2e0d
Chaitanya Ravuri <chaitu.ravuri.10@gmail.com>
2017-02-04 21:15:59 -0800

    Added Generate Problems
    yee

--- src/main/main.py
+++ src/main/main.py
@@ -1,64 +1,121 @@
-import urllib
+# import pytesseract
+# from PIL import Image
+# from PIL import ImageFilter
+
 import random
-import pytesseract
-from PIL import Image
-from PIL import ImageFilter
+import math
 
-def getProb(url):
-    page = urllib.urlopen(url).read()
-
-    probInd = page.find(
-        '<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr"><h2><span class="mw-headline" id="Problem">Problem</span></h2>')
-    pShift = 135
-    pEnd = -1
-    for i in range(probInd + pShift, len(page)):
-        if (page[i] == '?'):
-            pEnd = i + 1
-            break
-
-    problem = page[probInd + pShift:pEnd]
-    return problem
-
-def getAns(url):
-    page = urllib.urlopen(url).read()
-    ansInd = page.find(r'\boxed{\textbf{')
-
-    aBeg = 18
-    for i in range(ansInd, len(page)):
-        if (page[i] == '}'):
-            aBeg = i + 1
-            break
-
-    aEnd = -1
-    checkText = '}'
-    for i in range(aBeg, len(page)):
-        if (page[i:i + len(checkText)] == checkText):
-            aEnd = i
-
-    answer = page[aBeg: aEnd]
-    return(answer)
-
-def getRandomProb():
-    year = str(random.randint(2, 16))
-    prob = str(random.randint(1, 25))
-    level = random.randint(0, 1)
-    type = random.randint(0, 1)
-    if (len(year) == 1):
-        year = "0" + year
-    if (level == 0):
-        level = '10'
+def generateQuad():
+    [a, b, c] = [random.randint(1, 3), random.randint(1, 20), random.randint(1, 20)]
+    temp = []
+    temp.append(str(a) + "x^2 + " + str(b) + "x + " + str(c))
+    if (math.pow(b,2) - 4*a*c < 0):
+        temp.append("none")
     else:
-        level = '12'
-    if (type == 0):
-        type = 'A'
-    else:
-        type = 'B'
-    url = "http://artofproblemsolving.com/wiki/index.php/20" + year + "_AMC_" + level + type + "_Problems/Problem_" + prob
-    #url = "http://artofproblemsolving.com/wiki/index.php/2014_AMC_12B_Problems/Problem_9"
-    amc = [getProb(url), getAns(url)]
-    print url
-    print amc[0]
-    print amc[1]
+        answer1 = (-b + math.sqrt(math.pow(b, 2)-4*a*c))/2*a
+        answer2 = (-b - math.sqrt(math.pow(b, 2) - 4 * a * c)) / 2 * a
+        if (answer1 == answer2):
+            temp.append(str(answer1))
+        else:
+            temp.append(str(answer1))
+            temp.append(str(answer2))
+    return temp
+
+def generateLinear():
+    [a, b] = [random.randint(1, 25), random.randint(1, 25)]
+    temp = []
+    temp.append(str(a) + "x + " + str(b))
+    t = -b/a
+    temp.append(str(t))
+    return temp
+
+def determinant(result, rows, cols):
+    if rows == 2:
+        return result[0][0] * result[1][1] - result[0][1] * result[1][0]
+    determinant1 = 0
+    determinant2 = 0
+    for i in range(rows):
+        temp = 1
+        temp2 = 1
+        for j in range(cols):
+            temp *= result[(i + j) % cols][j]
+            temp2 *= result[(i + j) % cols][rows - 1 - j]
+        determinant1 += temp
+        determinant2 += temp2
+    return determinant1 - determinant2
+
+def generateSystem():
+    number = random.randint(2, 3)
+    if number == 2:
+        coefficients = []
+        for i in range(6):
+            coefficients.append(random.randint(1, 25))
+        [a, b, c, d, e, f] = coefficients
+        temp = [0, 0, 0, 0]
+        temp[0] = str(a) + "x + " + str(b) + "y = " + str(c)
+        temp[1] = str(d) + "x + " + str(e) + "y = " + str(f)
+        for i in range(6):
+            coefficients[i] *= 1.0
+        [a, b, c, d, e, f] = coefficients
+        t = (c * e - b * f) / a * e - b * d
+        temp[2] = str(t)
+        u = (c - a * t) / b
+        temp[3] = str(u)
+        return temp
+    if number == 3:
+        coefficients = []
+        for i in range(12):
+            coefficients.append(random.randint(1, 25))
+        [a, b, c, d, e, f, g, h, i, j, k, l] = coefficients
+        temp = [0, 0, 0, 0, 0, 0]
+        temp[0] = str(a) + "x + " + str(b) + "y + " + str(c) + "z = " + str(d)
+        temp[1] = str(e) + "x + " + str(f) + "y + " + str(g) + "z = " + str(h)
+        temp[2] = str(i) + "x + " + str(j) + "y + " + str(k) + "z = " + str(l)
+        for i in range(12):
+            coefficients[i] *= 1.0
+        [a, b, c, d, e, f, g, h, i, j, k, l] = coefficients
+        det = [[], [], []]
+        detx = [[], [], []]
+        dety = [[], [], []]
+        detz = [[], [], []]
+        det[0] = [a, b, c]
+        det[1] = [e, f, g]
+        det[2] = [i, j, k]
+        detx[0] = [d, b, c]
+        detx[1] = [h, f, g]
+        detx[2] = [l, j, k]
+        dety[0] = [a, d, c]
+        dety[1] = [e, h, g]
+        dety[2] = [i, l, k]
+        detz[0] = [a, b, d]
+        detz[1] = [e, f, h]
+        detz[2] = [i, j, l]
+        x1 = determinant(detx, 3, 3) / determinant(det, 3, 3)
+        y1 = determinant(dety, 3, 3) / determinant(det, 3, 3)
+        z1 = determinant(detz, 3, 3) / determinant(det, 3, 3)
+        temp[3] = str(x1)
+        temp[4] = str(y1)
+        temp[5] = str(z1)
+        return temp
+
+def getRandomProblem():
+    amc = "/Users/cravuri/Documents/harker/LSHacks/src/webCrawler/AMC.in"
+    f = open(amc, 'r')
+    lines = f.read().split('\n')
+    probnum = random.randint(0, len(lines) / 2 - 1)
+    return lines[2*probnum:2*probnum + 2]
+
+def generateRandomProblem():
+    probType = random.randint(0, 2)
+    prob = []
+    if (probType == 0):
+        prob = generateQuad()
+    if (probType == 1):
+        prob = generateLinear()
+    if (probType == 2):
+        prob = generateSystem()
+    prob.append(probType)
+    return prob
 
 def getText(img):
     image = Image.open(img)
@@ -66,4 +123,63 @@ def getText(img):
     text = pytesseract.image_to_string(image)
     return text
 
-getRandomProb()
+answerKey = []
+
+def normalize(prob):
+    normProb = ""
+    a = prob.split(" ")
+    curLen = 0
+    for i in range(len(a)):
+        normProb = normProb + a[i] + " "
+        curLen = curLen + len(a[i])
+        if (curLen >= 72):
+            normProb = normProb + "\n"
+            curLen = 0
+    return normProb
+
+def printCompetitionProblemSheet():
+    out = open("/Users/cravuri/Documents/harker/LSHacks/src/AMC.out", "w")
+    for i in range(1, 26):
+        prob = getRandomProblem()
+        out.write("Problem " + str(i) + ". " + normalize(prob[0]))
+        for _ in range(10):
+            out.write("\n")
+        out.write("Answer: " + "\n" + "\n")
+        answerKey.append(str(prob[1]))
+
+def printAlg1ProblemSheet():
+    out = open("/Users/cravuri/Documents/harker/LSHacks/src/AMC.out", "w")
+    for i in range(1, 16):
+        prob = generateRandomProblem()
+        type = prob[len(prob) - 1]
+        if type == 0:
+            out.write("Problem " + str(i) + ". Solve the following quadratic equation for x: \n" + normalize(prob[0]))
+            for _ in range(10):
+                out.write("\n")
+            out.write("Answer: " + "\n" + "\n")
+            if (len(prob) == 3):
+                answerKey.append(prob[1])
+            else:
+                answerKey.append(prob[1] + ", " + prob[2])
+        if type == 1:
+            out.write("Problem " + str(i) + ". Solve the following equation for x: \n" + normalize(prob[0]))
+            for _ in range(10):
+                out.write("\n")
+            out.write("Answer: " + "\n" + "\n")
+            answerKey.append(prob[1])
+        if type == 2:
+            if (len(prob) == 5):
+                out.write("Problem " + str(i) + ". Solve the following system of equations for x and y: \n" + normalize(prob[0]) + "\n" + normalize(prob[1]))
+                for _ in range(10):
+                    out.write("\n")
+                out.write("Answer: " + "\n" + "\n")
+                answerKey.append(str(prob[2]) + ", " + str(prob[3]))
+            if (len(prob) == 7):
+                out.write("Problem " + str(i) + ". Solve the following system of equations for x, y, and z: \n" + normalize(
+                    prob[0]) + "\n" + normalize(prob[1]) + "\n" + normalize(prob[2]))
+                for _ in range(10):
+                    out.write("\n")
+                out.write("Answer: " + "\n" + "\n")
+                answerKey.append(str(prob[3]) + ", " + str(prob[4]) + ", " + str(prob[5]))
+
+printAlg1ProblemSheet()
